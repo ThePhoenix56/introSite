4c9f2d508427178fc35e4134caebb6461f97f4f0
ThePhoenix56 <53459574+ThePhoenix56@users.noreply.github.com>
2024-02-09 02:43:01 +0100

    Add files via upload

--- phoenix562.js
+++ phoenix562.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded',function(event){
    
-    var dataText = [ "Hi! I'm Phoenix56." ];
+  var dataText = [ "Hi! I'm Phoenix56." ];
 
-    function skrivUt(text, i, fnCallback) {
-      if (i < (text.length)) {
-       document.getElementById("coolText").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+  function skrivUt(text, i, fnCallback) {
+    if (i < (text.length)) {
+     document.getElementById("coolText").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
 
+      setTimeout(function() {
+        skrivUt(text, i + 1, fnCallback)
+      }, 220);
+    }
+
+    else if (typeof fnCallback == 'function') {
+      setTimeout(fnCallback, 700);
+    }
+  }
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
         setTimeout(function() {
-          skrivUt(text, i + 1, fnCallback)
-        }, 220);
-      }
-
-      else if (typeof fnCallback == 'function') {
-        setTimeout(fnCallback, 700);
-      }
+          StartTextAnimation(0);
+        }, 40000);
+     }
+    if (i < dataText[i].length) {
+     skrivUt(dataText[i], 0, function(){
+       StartTextAnimation(i + 1);
+     });
     }
-     function StartTextAnimation(i) {
-       if (typeof dataText[i] == 'undefined'){
-          setTimeout(function() {
-            StartTextAnimation(0);
-          }, 40000);
-       }
-      if (i < dataText[i].length) {
-       skrivUt(dataText[i], 0, function(){
-         StartTextAnimation(i + 1);
-       });
-      }
-    }
-    StartTextAnimation(0);
-  });
+  }
+  StartTextAnimation(0);
+});
